8059ca1d5d5ae5aa1d9ad1b2e49a43215cc2705b
krychu <samp.krystian@gmail.com>
2020-01-17 23:17:00 +0100

    more new schema

--- scripts/ingest.pl
+++ scripts/ingest.pl
@@ -17,6 +17,8 @@ use JSON::Parse qw(parse_json);
 use Syntax::Keyword::Try;
 use Configuration qw($cfg);
 use Encode qw(decode encode);
+use Math::Round;
+use List::Util qw(min max);
 use Data::Dumper;
 use Log::Any::Adapter;
 use Log::Any '$log', prefix => '[ingest] ';
@@ -26,10 +28,14 @@ my $chartbl = quake_chartbl();
 
 try {
     my @data_dirs = ($cfg->{data_qtv}, $cfg->{data_badplace}, $cfg->{data_quake1pl});
+    my ($dbh, $sth_game) = prepare_dbh();
     for my $data_dir (@data_dirs) {
         my $abs_paths = get_stat_abs_paths($data_dir);
-        insert_stat_files($abs_paths);
+        insert_stat_files($abs_paths, $sth_game);
+        # my $abs_paths = get_stat_abs_paths($data_dir);
+        # insert_stat_files($abs_paths, $sth_game);
     }
+    finish_dbh($dbh, $sth_game);
 }
 catch {
     my $error = $@ || 'unknown error';
@@ -46,43 +52,262 @@ sub get_stat_abs_paths {
     return \@abs_paths;
 }
 
-sub insert_stat_files {
-    my $abs_paths = shift;
-
-    if (!scalar(@$abs_paths)) {
-        return;
-    }
-
+sub prepare_dbh {
     my $dbh = DBI->connect("dbi:Pg:host=$cfg->{postgresql_host};port=$cfg->{postgresql_port};dbname=$cfg->{postgresql_dbname}", $cfg->{postgresql_user}, '', {AutoCommit => 1, RaiseError => 1, PrintError => 1});
 
-    my $sth_game = $dbh->prepare(scalar(read_file('../sql/app/insert_game.sql')));
-    my $sth_game_player = $dbh->prepare(scalar(read_file('../sql/app/insert_game_player.sql')));
+    #my $sth_game = $dbh->prepare(scalar(read_file('../sql/app/insert_game.sql')));
+    #my $sth_game_player = $dbh->prepare(scalar(read_file('../sql/app/insert_game_player.sql')));
+    my $sth_game = prepare_sth_game($dbh);
+    return ($dbh, $sth_game);
+}
+
+sub finish_dbh {
+    my ($dbh, $sth_game) = @_;
+
+    $sth_game->finish or die "Cannot finish the game statement";
+    $dbh->disconnect or die "Disconnection error: $DBI::errstr\n";
+}
+
+sub insert_stat_files {
+    my ($abs_paths, $sth_game) = @_;
 
     my ($cnt, $skip_cnt) = (0, 0);
     for my $abs_path (@$abs_paths) {
         $log->info("Adding: $abs_path");
 
         my $txt = read_file($abs_path);
+
+        # skip if the format is unsupported
         if (!format_ok($txt)) {
             $skip_cnt++;
             next;
         }
-        $txt = decode_names($txt);
+
+        # txt pre-processing
+        decode_names($txt);
+
+        # json pre-processing
         my $json = parse_json($txt);
-        preprocess_date($json);
+        clean_dates($json);
+
         my $file = basename($abs_path);
         last if is_already_added($dbh, $file, $json);
 
-        my $pg_game_id = insert_game($sth_game, $json, $file);
-        map {insert_game_player($sth_game_player, $pg_game_id, $_)} @{$json->{players}};
+        my ($record_0, $record_1) = prepare_records($json, $file);
+        insert_game($record_0, $record_1, $file, $sth_game);
+
         $cnt++;
     }
-
     $log->info("New stat files added: $cnt, files skept: $skip_cnt (unsupported format)\n");
+}
 
-    $sth_game->finish or die "Cannot finish the game statement";
-    $sth_game_player->finish or die "Cannot finish the game_player statement";
-    $dbh->disconnect or die "Disconnection error: $DBI::errstr\n";
+sub insert_game {
+    my ($record_0, $record_1, $file, $sth_game) = @_;
+
+    for my $r ($record_0, $record_1) {
+        $sth->execute(
+            @{$r}{
+                qw(date
+                   map
+                   hostname
+                   mode
+                   tl
+                   dm
+                   duration
+                   demo
+                   file
+
+                   a_name
+                   a_top_color
+                   a_bottom_color
+                   a_ping
+                   a_frags
+                   a_spawn_frags
+                   a_suicides
+                   a_kills
+                   a_deaths
+                   a_spree_max
+                   a_dmg_given
+                   a_dmg_taken
+                   a_rl_attacks
+                   a_rl_hits
+                   a_rl_virtual
+                   a_rl_kills
+                   a_rl_deaths
+                   a_rl_dmg_given
+                   a_rl_dmg_taken
+                   a_lg_attacks
+                   a_lg_hits
+                   a_lg_kills
+                   a_lg_deaths
+                   a_lg_dmg_given
+                   a_lg_dmg_taken
+                   a_ra
+                   a_ya
+                   a_mh
+                   a_speed_avg
+
+                   b_name
+                   b_top_color
+                   b_bottom_color)}
+            );
+    }
+}
+
+sub prepare_records {
+    my ($json, $file) = @_;
+
+    my ($a_json, $b_json) = @{ $json->{players} };
+
+    my $record_0 = {
+        date                 => $json->{date},
+        map                  => $json->{map},
+        hostname             => $json->{hostname},
+        mode                 => $json->{mode},
+        tl                   => $json->{tl},
+        dm                   => $json->{dm},
+        duration             => $json->{duration},
+        demo                 => $json->{demo},
+        file                 => $json->{file},
+
+        a_name               => $a_json->{name},
+        a_top_color          => $a_json->{'top-color'},
+        a_bottom_color       => $a_json->{'bottom-color'},
+        a_ping               => $a_json->{ping},
+        a_frags              => $a_json->{stats}{frags},
+        a_spawn_frags        => $a_json->{stats}{'spawn-frags'},
+        a_suicides           => $a_json->{stats}{suicides},
+        a_kills              => $a_json->{stats}{kills},
+        a_deaths             => $a_json->{stats}{deaths},
+        a_spree_max          => $a_json->{spree}{max},
+        a_dmg_given          => $a_json->{dmg}{given}, # should it subtract self?
+        a_dmg_taken          => $a_json->{dmg}{taken},
+        a_rl_attacks         => $a_json->{weapons}{rl}{acc}{attacks},
+        a_rl_hits            => $a_json->{weapons}{rl}{acc}{hits},
+        a_rl_virtual         => $a_json->{weapons}{rl}{acc}{virtual},
+        a_rl_kills           => $a_json->{weapons}{rl}{kills}{enemy}, # what is total and self here?
+        a_rl_deaths          => $a_json->{weapons}{rl}{deaths},
+        a_rl_dmg_given       => $a_json->{weapons}{rl}{damage}{enemy},
+        a_rl_dmg_taken       => $b_json->{weapons}{rl}{damage}{enemy},
+        a_lg_attacks         => $a_json->{weapons}{lg}{acc}{attacks},
+        a_lg_hits            => $a_json->{weapons}{lg}{acc}{hits},
+        a_lg_kills           => $a_json->{weapons}{lg}{kills}{enemy},
+        a_lg_deaths          => $a_json->{weapons}{lg}{deaths},
+        a_lg_dmg_given       => $a_json->{weapons}{lg}{damage}{enemy},
+        a_lg_dmg_taken       => $b_json->{weapons}{lg}{damage}{enemy},
+        a_ra                 => $a_json->{items}{ra}{took},
+        a_ya                 => $a_json->{items}{ya}{took},
+        a_mh                 => $a_json->{items}{health_100}{took},
+        a_speed_avg          => $a_json->{speed}{avg},
+
+        b_name               => $b_json->{name},
+        b_top_color          => $b_json->{'top-color'},
+        b_bottom_color       => $b_json->{'bottom-color'},
+        b_ping               => $b_json->{ping},
+        b_frags              => $b_json->{stats}{frags},
+        b_spawn_frags        => $b_json->{stats}{'spawn-frags'},
+        b_suicides           => $b_json->{stats}{suicides},
+        b_kills              => $b_json->{stats}{kills},
+        b_deaths             => $b_json->{stats}{deaths},
+        b_spree_max          => $b_json->{spree}{max},
+        b_dmg_given          => $b_json->{dmg}{given}, # should it subtract self?
+        b_dmg_taken          => $b_json->{dmg}{taken},
+        b_rl_attacks         => $b_json->{weapons}{rl}{acc}{attacks},
+        b_rl_hits            => $b_json->{weapons}{rl}{acc}{hits},
+        b_rl_virtual         => $b_json->{weapons}{rl}{acc}{virtual},
+        b_rl_kills           => $b_json->{weapons}{rl}{kills}{enemy}, # what is total and self here?
+        b_rl_deaths          => $b_json->{weapons}{rl}{deaths},
+        b_rl_dmg_given       => $b_json->{weapons}{rl}{damage}{enemy},
+        b_rl_dmg_taken       => $a_json->{weapons}{rl}{damage}{enemy},
+        b_lg_attacks         => $b_json->{weapons}{lg}{acc}{attacks},
+        b_lg_hits            => $b_json->{weapons}{lg}{acc}{hits},
+        b_lg_kills           => $b_json->{weapons}{lg}{kills}{enemy},
+        b_lg_deaths          => $b_json->{weapons}{lg}{deaths},
+        b_lg_dmg_given       => $b_json->{weapons}{lg}{damage}{enemy},
+        b_lg_dmg_taken       => $a_json->{weapons}{lg}{damage}{enemy},
+        b_ra                 => $b_json->{items}{ra}{took},
+        b_ya                 => $b_json->{items}{ya}{took},
+        b_mh                 => $b_json->{items}{health_100}{took},
+        b_speed_avg          => $b_json->{speed}{avg}
+    }
+}
+
+sub prepare_sth_game {
+    return $dbh->prepare(
+        q{
+        INSERT INTO games (
+          date,
+          map,
+          hostname,
+          mode,
+          tl,
+          dm,
+          duration,
+          demo,
+          file,
+
+          a_name,
+          a_top_color,
+          a_bottom_color,
+          a_ping,
+          a_frags,
+          a_spawn_frags,
+          a_suicides,
+          a_kills,
+          a_deaths,
+          a_spree_max,
+          a_dmg_given,
+          a_dmg_taken,
+          a_rl_attacks,
+          a_rl_hits,
+          a_rl_virtual,
+          a_rl_kills,
+          a_rl_deaths,
+          a_rl_dmg_given,
+          a_rl_dmg_taken,
+          a_lg_attacks,
+          a_lg_hits,
+          a_lg_kills,
+          a_lg_deaths,
+          a_lg_dmg_given,
+          a_lg_dmg_taken,
+          a_ra,
+          a_ya,
+          a_mh,
+          a_speed_avg,
+
+          b_name,
+          b_top_color,
+          b_bottom_color,
+          b_ping,
+          b_frags,
+          b_spawn_frags,
+          b_suicides,
+          b_kills,
+          b_deaths,
+          b_spree_max,
+          b_dmg_given,
+          b_dmg_taken,
+          b_rl_attacks,
+          b_rl_hits,
+          b_rl_virtual,
+          b_rl_kills,
+          b_rl_deaths,
+          b_rl_dmg_given,
+          b_rl_dmg_taken,
+          b_lg_attacks,
+          b_lg_hits,
+          b_lg_kills,
+          b_lg_deaths,
+          b_lg_dmg_given,
+          b_lg_dmg_taken,
+          b_ra,
+          b_ya,
+          b_mh,
+          b_speed_avg
+        )
+        VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24,$25,$26,$27,$28,$29,$30,$31,$32,$33,$34,$35,$36,$37,$38,$39,$40,$41,$42,$43,$44,$45,$46,$47,$48,$49,$50,$51,$52,$53,$54,$55,$56,$57,$58,$59,$60,$61,$62,$63,$64,$65,$66,$67);
+        });
 }
 
 sub format_ok {
@@ -138,7 +363,7 @@ sub insert_game {
     return $sth->fetch()->[0];
 }
 
-sub preprocess_date {
+sub clean_dates {
     my $json = shift;
 
     my $date = $json->{date};
--- sql/schema.sql
+++ sql/schema.sql
@@ -1,9 +1,10 @@
-DROP TABLE IF EXISTS game_players;
 DROP TABLE IF EXISTS games;
-DROP SEQUENCE IF EXISTS game_players_id_seq;
 DROP SEQUENCE IF EXISTS games_id_seq;
 
-CREATE TABLE duels
+-- Important note: each game is two rows, each with players swapped. This is so
+-- that you can query for a_name = X rows and get all games of player X.
+CREATE SEQUENCE games_id_seq;
+CREATE TABLE games
 (
   id                   INTEGER PRIMARY KEY DEFAULT nextval('duels_id_seq'),
   date                 TIMESTAMP,
@@ -17,6 +18,7 @@ CREATE TABLE duels
   file                 TEXT,
   created_at           TIMESTAMP DEFAULT ( now() AT TIME ZONE 'utc' ),
 
+  -- player a
   a_name               TEXT,
   a_top_color          INTEGER,
   a_bottom_color       INTEGER,
@@ -24,143 +26,157 @@ CREATE TABLE duels
   a_frags              INTEGER,
   a_spawn_frags        INTEGER,
   a_suicides           INTEGER,
-  a_kill_death         REAL,
-  a_frags_min          REAL,
+  a_kills              INTEGER,
+  a_deaths             INTEGER,
   a_spree_max          INTEGER,
-  a_dmg_percent        INTEGER,
-  a_dmg_min            INTEGER,
-  a_dmg_given_taken    REAL,
-  a_rl_dmg_min         INTEGER,
-  a_rl_dmg_given_taken REAL,
+  a_dmg_given          INTEGER,
+  a_dmg_taken          INTEGER,
   a_rl_attacks         INTEGER,
   a_rl_hits            INTEGER,
   a_rl_virtual         INTEGER,
-  a_lg_dmg_min         INTEGER,
-  a_lg_acc             INTEGER,
+  a_rl_kills           INTEGER,
+  a_rl_deaths          INTEGER,
+  a_rl_dmg_given       INTEGER,
+  a_rl_dmg_taken       INTEGER,
+  a_lg_attacks         INTEGER,
+  a_lg_hits            INTEGER,
+  a_lg_kills           INTEGER,
+  a_lg_deaths          INTEGER,
+  a_lg_dmg_given       INTEGER,
+  a_lg_dmg_taken       INTEGER,
   a_ra                 INTEGER,
   a_ya                 INTEGER,
   a_mh                 INTEGER,
   a_speed_avg          INTEGER,
 
-HERE
-
-
-a_name               TEXT,
-  a_top_color          INTEGER,
-  a_bottom_color       INTEGER,
-  a_ping               INTEGER,
-  a_frags              INTEGER,
-  a_kills              INTEGER,
-  a_deaths             INTEGER,
-  a_spawn_frags        INTEGER,
-  a_suicides           INTEGER,
-  a_dmg_taken          INTEGER,
-  a_dmg_given          INTEGER,
-  a_speed_avg          INTEGER,
-  a_ra                 INTEGER,
-  a_ya                 INTEGER,
-  a_mh                 INTEGER,
-  a_rl_attacks         INTEGER,
-  a_rl_dmg             INTEGER,
-  a_rl_kills
+  -- player b
+  b_name               TEXT,
+  b_top_color          INTEGER,
+  b_bottom_color       INTEGER,
+  b_ping               INTEGER,
+  b_frags              INTEGER,
+  b_spawn_frags        INTEGER,
+  b_suicides           INTEGER,
+  b_kills              INTEGER,
+  b_deaths             INTEGER,
+  b_spree_max          INTEGER,
+  b_dmg_given          INTEGER,
+  b_dmg_taken          INTEGER,
+  b_rl_attacks         INTEGER,
+  b_rl_hits            INTEGER,
+  b_rl_virtual         INTEGER,
+  b_rl_kills           INTEGER,
+  b_rl_deaths          INTEGER,
+  b_rl_dmg_given       INTEGER,
+  b_rl_dmg_taken       INTEGER,
+  b_lg_attacks         INTEGER,
+  b_lg_hits            INTEGER,
+  b_lg_kills           INTEGER,
+  b_lg_deaths          INTEGER,
+  b_lg_dmg_given       INTEGER,
+  b_lg_dmg_taken       INTEGER,
+  b_ra                 INTEGER,
+  b_ya                 INTEGER,
+  b_mh                 INTEGER,
+  b_speed_avg          INTEGER
 );
 
-CREATE SEQUENCE games_id_seq;
-CREATE TABLE games
-(
-  id                   INTEGER PRIMARY KEY DEFAULT nextval('games_id_seq'),
-  version              INTEGER,
-  date                 TIMESTAMP,
-  map                  TEXT,
-  hostname             TEXT,
-  ip                   TEXT,
-  port                 INTEGER,
-  mode                 TEXT,
-  tl                   INTEGER,
-  dm                   INTEGER,
-  duration             INTEGER,
-  demo                 TEXT,
-  file                 TEXT,
-  created_at           TIMESTAMP DEFAULT ( now() AT TIME ZONE 'utc' )
-);
+-- CREATE SEQUENCE games_id_seq;
+-- CREATE TABLE games
+-- (
+--   id                   INTEGER PRIMARY KEY DEFAULT nextval('games_id_seq'),
+--   version              INTEGER,
+--   date                 TIMESTAMP,
+--   map                  TEXT,
+--   hostname             TEXT,
+--   ip                   TEXT,
+--   port                 INTEGER,
+--   mode                 TEXT,
+--   tl                   INTEGER,
+--   dm                   INTEGER,
+--   duration             INTEGER,
+--   demo                 TEXT,
+--   file                 TEXT,
+--   created_at           TIMESTAMP DEFAULT ( now() AT TIME ZONE 'utc' )
+-- );
 
-CREATE SEQUENCE game_players_id_seq;
-CREATE TABLE game_players
-(
-  id                   INTEGER PRIMARY KEY DEFAULT nextval('game_players_id_seq'),
-  game_id              INTEGER REFERENCES games (id),
-  top_color            INTEGER,
-  bottom_color         INTEGER,
-  ping                 INTEGER,
-  name                 TEXT,
-  -- team                 TEXT,
+-- CREATE SEQUENCE game_players_id_seq;
+-- CREATE TABLE game_players
+-- (
+--   id                   INTEGER PRIMARY KEY DEFAULT nextval('game_players_id_seq'),
+--   game_id              INTEGER REFERENCES games (id),
+--   top_color            INTEGER,
+--   bottom_color         INTEGER,
+--   ping                 INTEGER,
+--   name                 TEXT,
+--   -- team                 TEXT,
 
-  -- stats
-  frags                INTEGER,
-  deaths               INTEGER,
-  -- tk                   INTEGER,
-  spawn_frags          INTEGER,
-  kills                INTEGER,
-  suicides             INTEGER,
+--   -- stats
+--   frags                INTEGER,
+--   deaths               INTEGER,
+--   -- tk                   INTEGER,
+--   spawn_frags          INTEGER,
+--   kills                INTEGER,
+--   suicides             INTEGER,
 
-  -- dmg
-  damage_taken         INTEGER,
-  damage_given         INTEGER,
-  --damage_team          INTEGER,
-  damage_self          INTEGER,
-  -- dmg_team_weapons     INTEGER,
-  -- dmg_enemy_weapons    INTEGER,
+--   -- dmg
+--   damage_taken         INTEGER,
+--   damage_given         INTEGER,
+--   --damage_team          INTEGER,
+--   damage_self          INTEGER,
+--   -- dmg_team_weapons     INTEGER,
+--   -- dmg_enemy_weapons    INTEGER,
 
-  -- xfer                 INTEGER,
-  spree_max            INTEGER,
-  spree_quad           INTEGER,
-  control              REAL,
-  speed_max            REAL,
-  speed_avg            REAL,
+--   -- xfer                 INTEGER,
+--   spree_max            INTEGER,
+--   spree_quad           INTEGER,
+--   control              REAL,
+--   speed_max            REAL,
+--   speed_avg            REAL,
 
-  -- items
-  health_15            INTEGER,
-  health_25            INTEGER,
-  health_100           INTEGER,
-  ga                   INTEGER,
-  ya                   INTEGER,
-  ra                   INTEGER,
-  q                    INTEGER,
-  q_time               INTEGER,
+--   -- items
+--   health_15            INTEGER,
+--   health_25            INTEGER,
+--   health_100           INTEGER,
+--   ga                   INTEGER,
+--   ya                   INTEGER,
+--   ra                   INTEGER,
+--   q                    INTEGER,
+--   q_time               INTEGER,
 
-  -- weapons
-  sg_attacks           INTEGER,
-  sg_direct_hits       INTEGER,
-  sg_kills             INTEGER,
-  sg_deaths            INTEGER,
-  sg_damage            INTEGER,
+--   -- weapons
+--   sg_attacks           INTEGER,
+--   sg_direct_hits       INTEGER,
+--   sg_kills             INTEGER,
+--   sg_deaths            INTEGER,
+--   sg_damage            INTEGER,
 
-  gl_attacks           INTEGER,
-  gl_direct_hits       INTEGER,
-  gl_effective_hits    INTEGER,
-  gl_kills             INTEGER,
-  -- gl_kills_owner       INTEGER,
-  gl_deaths            INTEGER,
-  gl_picked            INTEGER,
-  -- gl_dropped           INTEGER,
-  gl_damage            INTEGER,
+--   gl_attacks           INTEGER,
+--   gl_direct_hits       INTEGER,
+--   gl_effective_hits    INTEGER,
+--   gl_kills             INTEGER,
+--   -- gl_kills_owner       INTEGER,
+--   gl_deaths            INTEGER,
+--   gl_picked            INTEGER,
+--   -- gl_dropped           INTEGER,
+--   gl_damage            INTEGER,
 
-  rl_attacks           INTEGER,
-  rl_direct_hits       INTEGER,
-  rl_effective_hits    INTEGER,
-  rl_kills             INTEGER,
-  rl_deaths            INTEGER,
-  rl_picked            INTEGER,
-  rl_damage            INTEGER,
+--   rl_attacks           INTEGER,
+--   rl_direct_hits       INTEGER,
+--   rl_effective_hits    INTEGER,
+--   rl_kills             INTEGER,
+--   rl_deaths            INTEGER,
+--   rl_picked            INTEGER,
+--   rl_damage            INTEGER,
 
 
-  lg_attacks           INTEGER,
-  lg_direct_hits       INTEGER,
-  lg_kills             INTEGER,
-  lg_deaths            INTEGER,
-  lg_picked            INTEGER,
-  lg_damage            INTEGER,
+--   lg_attacks           INTEGER,
+--   lg_direct_hits       INTEGER,
+--   lg_kills             INTEGER,
+--   lg_deaths            INTEGER,
+--   lg_picked            INTEGER,
+--   lg_damage            INTEGER,
 
-  created_at           TIMESTAMP DEFAULT ( now() AT TIME ZONE 'utc' )
-);
+--   created_at           TIMESTAMP DEFAULT ( now() AT TIME ZONE 'utc' )
+-- );
 
